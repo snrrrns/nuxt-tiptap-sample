d19e2f6cb695f1c96dbfc587b0ae2ebcb79f8332
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2023-05-27 11:21:25 +0900

    Update: added editor name cursor

--- components/TiptapEditor.vue
+++ components/TiptapEditor.vue
@@ -6,6 +6,7 @@
 import { Editor, EditorContent } from '@tiptap/vue-3';
 import StarterKit from '@tiptap/starter-kit';
 import Collaboration from '@tiptap/extension-collaboration';
+import CollaborationCursor from '@tiptap/extension-collaboration-cursor';
 import { WebsocketProvider } from 'y-websocket';
 import * as Y from 'yjs';
 import { ShallowRef } from 'vue';
@@ -32,7 +33,12 @@ export default {
   },
   mounted() {
     const ydoc = new Y.Doc;
-    this.provider = new WebsocketProvider('ws://localhost:1234', 'sample-document', ydoc);
+
+    this.provider = new WebsocketProvider(
+      'ws://localhost:1234',
+      'sample-document',
+      ydoc)
+    ;
 
     this.editor = new Editor({
       content: '',
@@ -41,6 +47,13 @@ export default {
         Collaboration.configure({
           document: ydoc,
         }),
+        CollaborationCursor.configure({
+          provider: this.provider,
+          user: {
+            name: this.getRandomName(),
+            color: this.getRandomColor(),
+          }
+        }),
       ],
     });
   },
@@ -48,10 +61,57 @@ export default {
     this.editor?.destroy();
     this.provider?.destroy();
   },
+  methods: {
+    getRandomColor(): string {
+      const list = [
+        '#958DF1',
+        '#F98181',
+        '#FBBC88',
+        '#FAF594',
+        '#70CFF8',
+        '#94FADB',
+        '#B9F18D',
+      ];
+      return list[Math.floor(Math.random() * list.length)];
+    },
+
+    getRandomName(): string {
+      const list = [
+        'Lea Thompson',
+        'Cyndi Lauper',
+        'Tom Cruise',
+        'Madonna',
+        'Jerry Hall',
+        'Joan Collins',
+        'Winona Ryder',
+        'Christina Applegate',
+        'Alyssa Milano',
+        'Molly Ringwald',
+        'Ally Sheedy',
+        'Debbie Harry',
+        'Ally Sheedy',
+        'Olivia Newton-John',
+        'Elton John',
+        'Micheal J. Fox',
+        'Axl Rose',
+        'Emilio Estevez',
+        'Ralph Macchio',
+        'Rob Lowe',
+        'Jennifer Grey',
+        'Mickey Rourke',
+        'John Cusack',
+        'Matthew Broderick',
+        'Justine Bateman',
+        'Lisa Bonet',
+      ];
+      return list[Math.floor(Math.random() * list.length)];
+    }
+  }
 }
 </script>
 
 <style lang="scss">
+/* Basic editor styles */
 .ProseMirror {
   > * + * {
     margin-top: 0.75em;
@@ -59,30 +119,110 @@ export default {
   border: 1px solid #e0e0e0 !important;
   box-sizing: border-box;
   border-radius: 3px;
-  padding: 16px;
-  font-size: 14px;
+  padding: 16px 16px 16px 16px;
+
+  ul,
+  ol {
+    padding: 0 1rem;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    line-height: 1.1;
+  }
+
   code {
-    background-color: rbga(#616161, 0.1);
+    background-color: rgba(#616161, 0.1);
     color: #616161;
   }
+
+  pre {
+    background: #0d0d0d;
+    color: #fff;
+    font-family: "JetBrainsMono", monospace;
+    padding: 0.75rem 1rem;
+    border-radius: 0.5rem;
+
+    code {
+      color: inherit;
+      padding: 0;
+      background: none;
+      font-size: 0.8rem;
+    }
+  }
+
+  mark {
+    background-color: #faf594;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  hr {
+    margin: 1rem 0;
+  }
+
+  blockquote {
+    padding-left: 1rem;
+    border-left: 2px solid rgba(#0d0d0d, 0.1);
+  }
+
+  hr {
+    border: none;
+    border-top: 2px solid rgba(#0d0d0d, 0.1);
+    margin: 2rem 0;
+  }
+
+  ul[data-type="taskList"] {
+    list-style: none;
+    padding: 0;
+
+    li {
+      display: flex;
+      align-items: center;
+
+      > label {
+        flex: 0 0 auto;
+        margin-right: 0.5rem;
+        user-select: none;
+      }
+
+      > div {
+        flex: 1 1 auto;
+      }
+    }
+  }
+}
+/* Give a remote user a caret */
+.collaboration-cursor__caret {
+  position: relative;
+  margin-left: -1px;
+  margin-right: -1px;
+  border-left: 1px solid #0d0d0d;
+  border-right: 1px solid #0d0d0d;
+  word-break: normal;
+  pointer-events: none;
 }
 
-.content {
-  padding: 1rem 0 0;
-  h3 {
-    margin: 1rem 0 0.5rem;
-  }
-  pre {
-    border-radius: 5px;
-    color: #333;
-  }
-  code {
-    display: block;
-    white-space: pre-wrap;
-    font-size: 0.8rem;
-    padding: 0.75rem 1rem;
-    background-color: #e0ecef;
-    color: #495057;
-  }
+/* Render the username above the caret */
+.collaboration-cursor__label {
+  position: absolute;
+  top: -1.4em;
+  left: -1px;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  user-select: none;
+  color: #0d0d0d;
+  padding: 0.1rem 0.3rem;
+  border-radius: 3px 3px 3px 0;
+  white-space: nowrap;
 }
 </style>
